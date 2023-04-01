e01b9300ae2efd5b6a0b0fc9ecff7e4d6ab82a37
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-04-01 22:45:58 +0300

    first test

--- src/test/java/week1/FirstTest.java
+++ src/test/java/week1/FirstTest.java
@@ -7,13 +7,70 @@ import org.junit.jupiter.api.*;
 
 public class FirstTest {
 
+
+    // test status code and headers
     @Test
     public void test1(){
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .when().get("http://3.216.30.92:8000/api/spartans");
 
         System.out.println(response.statusCode());
-        Assertions.assertEquals(200,response.statusCode());
-
+        Assertions.assertEquals(200,response.statusCode(),"Test status code");
+        Assertions.assertEquals("application/json",response.header("Content-Type"));
+//        response.prettyPrint();
+        Assertions.assertEquals("application/json",response.contentType());
     }
+
+    // get single spartan and assert body values
+    @Test
+    public void test2(){
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get("http://3.216.30.92:8000/api/spartans/30");
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+
+        System.out.println(response.path("id").toString());
+        System.out.println(response.path("name").toString());
+        System.out.println(response.path("gender").toString());
+        System.out.println(response.path("phone").toString());
+
+        Assertions.assertEquals("30",response.path("id").toString());
+        Assertions.assertEquals("Melania",response.path("name").toString());
+    }
+
+    // search by name
+    @Test
+    public void test3(){
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .and().queryParam("nameContains","Da")
+                .when().get("http://3.216.30.92:8000/api/spartans/search");
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+    }
+
+    // search by gender
+    @Test
+    public void test4(){
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .and().queryParam("gender","Male")
+                .when().get("http://3.216.30.92:8000/api/spartans/search");
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+    }
+
+    // search by name and gender together
+    @Test
+    public void test5(){
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .and().queryParam("gender","Male")
+                .and().queryParam("nameContains","e")
+                .when().get("http://3.216.30.92:8000/api/spartans/search");
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+    }
+
 }
