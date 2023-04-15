9b0e7ec367219ba68f123e147345602559b43f3d
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-04-15 18:38:59 +0300

    week 3 second push

--- src/test/java/week3/PostMethod.java
+++ src/test/java/week3/PostMethod.java
@@ -4,6 +4,9 @@ import io.restassured.http.*;
 import io.restassured.response.*;
 import org.junit.jupiter.api.*;
 import week2.*;
+import week2.POJO.*;
+
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -25,6 +28,42 @@ public class PostMethod extends TestBase {
                 .when().post("/api/spartans");
 
         response.prettyPrint();
+        Assertions.assertEquals(201, response.statusCode());
+        Assertions.assertEquals("A Spartan is Born!",response.path("success"));
+    }
+
+    // provide body as a map
+    @Test
+    public void test2(){
+        Map<String, Object> bodyMap = new HashMap();
+        bodyMap.put("gender","Male");
+        bodyMap.put("name","Mike");
+        bodyMap.put("phone",5286394175L);
+
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().body(bodyMap)
+                .when().post("/api/spartans");
+
+        Assertions.assertEquals(201, response.statusCode());
+        Assertions.assertEquals("A Spartan is Born!",response.path("success"));
+
+    }
+
+    // providing body as POJO object
+    @Test
+    public void test3(){
+        Spartan spartanBody = new Spartan();
+        spartanBody.setGender("Male");
+        spartanBody.setName("Harry");
+        spartanBody.setPhone(5286394175L);
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().body(spartanBody)
+                .when().post("/api/spartans");
+
         Assertions.assertEquals(201, response.statusCode());
         Assertions.assertEquals("A Spartan is Born!",response.path("success"));
     }
