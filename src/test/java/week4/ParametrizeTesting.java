bd58077d85d4fcbd1a326cfc9f2ff85043da954f
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-04-22 19:17:02 +0300

    week 4 third push

--- src/test/java/week4/ParametrizeTesting.java
+++ src/test/java/week4/ParametrizeTesting.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 import week2.*;
 
+import java.util.*;
+
 public class ParametrizeTesting extends TestBase {
 
     /*
@@ -21,11 +23,41 @@ public class ParametrizeTesting extends TestBase {
 
 
     @ParameterizedTest
-    @ValueSource(ints = {4,6,7,8,9})
-    @Test
-    public void test1(int id){
+    @ValueSource(ints = {4, 6, 7, 8, 9})
+    public void valueSource(int id) {
         Response response = RestAssured.given().accept(ContentType.JSON)
-                .when().get("http://3.216.30.92:8000/api/spartans/"+ id + "");
+                .when().get("http://3.216.30.92:8000/api/spartans/" + id + "");
         response.prettyPrint();
     }
+
+
+    public static List<String> getPartialNames() {
+        List<String> names = Arrays.asList("va", "ma", "de", "nu");
+        return names;
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("getPartialNames")
+    public void methodSource(String names) {
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .and().queryParam("nameContains", names)
+                .when().get("http://3.216.30.92:8000/api/spartans/search");
+
+        response.prettyPrint();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"8,Rodolfo",
+               "13,Jaimie",
+               "21,Mark"})
+    public void csvSource(int id, String name) {
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .pathParam("id", id)
+                .when().get("/api/spartans/{id}");
+
+        Assertions.assertEquals(name, response.path("name"));
+    }
+
+
 }
